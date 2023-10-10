d4f3f2114977eb77b692cf5754a413f8533cf009
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2023-10-10 11:41:51 +0300

    change name of board

--- src/test/java/TestCases/end2end.java
+++ src/test/java/TestCases/end2end.java
@@ -17,7 +17,7 @@ public class end2end {
     @Test(priority = 0)
     public void CreateBoard()
     {
-        Response createBoard = EndPoints.endPoints.createBoard("board.v1");
+        Response createBoard = EndPoints.endPoints.createBoard("board.v2");
 
         createBoard.prettyPrint() ;
           // trelloDe_Serialization boardInfo = createBoard.body().as(trelloDe_Serialization.class);
